caf4a82be670f6e37276a5dab7628beef980424e
HoangDL <dlhoang1997@gmail.com>
2025-09-08 10:19:07 +0700

    base

--- app/Models/Mongo/Customers.php
+++ app/Models/Mongo/Customers.php
@@ -3,8 +3,18 @@
 namespace App\Models\Mongo;
 
 use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\SoftDeletes;
 
 class Customers extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $connection = "mongodb";
     protected $table = 'customers';
+
+    protected $fillable = [
+
+    ];
+    protected $primaryKey = "_id";
 }
