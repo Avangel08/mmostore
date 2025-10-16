90a53639d9bd795aee9a7031b71e7c3c33146403
HoangDL <dlhoang1997@gmail.com>
2025-10-16 16:21:24 +0700

    base

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\Seller\Product\SellerAccountApiController;
+use App\Http\Controllers\Api\Buyer\Product\ProductController;
+use App\Http\Controllers\Api\Buyer\Order\OrderController;
 use App\Http\Controllers\Api\Webhook\SePay\SePayWebHookController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +24,20 @@ Route::group(['prefix' => 'v1','as' => 'api.'], function () use ($mainDomain) {
             Route::group(['prefix' => 'accounts', 'as' => 'seller.'], function () {
                 Route::post("/", [SellerAccountApiController::class, 'store'])->name('accounts.store');
                 Route::delete("/", [SellerAccountApiController::class, 'destroy'])->name('accounts.destroy');
+            });
+        });
+
+        // buyer api
+        Route::middleware((['validate.subdomain', 'tenant.mongo', 'auth:buyer_api', 'validate.buyer.token']))->group(function () {
+            Route::group(['prefix' => 'product', 'as' => 'buyer.'], function () {
+                Route::get('/', [ProductController::class, 'index'])->name('product.index');
+                Route::get('/{id}', [ProductController::class, 'show'])->name('product.show');
+            });
+
+            Route::group(['prefix' => 'order', 'as' => 'buyer.'], function () {
+                Route::get('/', [OrderController::class, 'index'])->name('order.index');
+                Route::get('/{orderNumber}', [OrderController::class, 'show'])->name('order.show');
+                Route::post('/checkout', [OrderController::class, 'checkout'])->name('order.checkout');
             });
         });
 
