366bca53b7078a56b90993488308cf9a6cae88e7
Trung Kiên <kiennt@hachitechsolution.com>
2025-10-22 17:04:32 +0700

    t102653 | add meta + fix setting meta desc

--- resources/js/Pages/Home/index.tsx
+++ resources/js/Pages/Home/index.tsx
@@ -19,7 +19,31 @@ export default function Index() {
 
     return (
         <React.Fragment>
-            <Head title="MMO Shop" />
+            <Head>
+                <title>MMO Shop</title>
+                <meta
+                    name="description"
+                    content="Tạo shop MMO của riêng bạn trong 5 phút"
+                />
+                <meta name="author" content="MMO Shop" />
+
+                {/* Open Graph meta tags */}
+                <meta property="og:title" content="MMO Shop" />
+                <meta property="og:description" content="Tạo shop MMO của riêng bạn trong 5 phút" />
+                {/* <meta property="og:image" content={theme?.pageHeaderImage || ""} /> */}
+                <meta property="og:url" content="https://mmostore.local" />
+                <meta property="og:type" content="product" />
+                <meta property="og:site_name" content="MMO Shop" />
+
+                {/* Twitter card tags (tùy chọn) */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="MMO Shop" />
+                <meta name="twitter:description" content="Tạo shop MMO của riêng bạn trong 5 phút" />
+                {/* <meta name="twitter:image" content={theme?.pageHeaderImage || ""} /> */}
+
+                <meta name="robots" content="index, follow" />
+            </Head>
+
             <div className="layout-wrapper landing">
                 <Navbar />
                 <Home />
